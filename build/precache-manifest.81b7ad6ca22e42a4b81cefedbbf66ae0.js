4c999cee8f1b440fd73e077b756b1196475ba0b2
Kamal BRI DSE <kamal@Kamals-MacBook-Pro.local>
2020-12-10 22:48:00 +0700

    build folder

---
+++ build/precache-manifest.81b7ad6ca22e42a4b81cefedbbf66ae0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "316ad9e32f4696daab925ed8586810a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "903bcbc1d34b66819320",
+    "url": "/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "43f1b0c25fdf3110463c",
+    "url": "/static/css/main.6777df22.chunk.css"
+  },
+  {
+    "revision": "903bcbc1d34b66819320",
+    "url": "/static/js/2.14b38c31.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.14b38c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43f1b0c25fdf3110463c",
+    "url": "/static/js/main.c62a4883.chunk.js"
+  },
+  {
+    "revision": "9d9a4c99211d98368a0d",
+    "url": "/static/js/runtime-main.7b3982eb.js"
+  }
+]);
